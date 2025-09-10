0ac63c7babe6e892a311508c89748e34d0b10e87
YANNICK kabunga <yankbg2@gmail.com>
2025-09-11 01:23:10 +0300

    add JListCardLayout class with CardLayout and navigation buttons

--- src/main/java/JListCardLayout.java
+++ src/main/java/JListCardLayout.java
@@ -1,2 +1,16 @@
-public class JListCardLayout {
+import javax.swing.*;
+import java.awt.*;
+
+public class JListCardLayout extends JFrame {
+    CardLayout cardLayout = new CardLayout();
+
+    // Declaration of JPanel
+    JPanel mainPanel = new JPanel(cardLayout);
+    JPanel card1 = new JPanel();
+    JPanel card2 = new JPanel();
+    JPanel card3 = new JPanel();
+    JButton btn1 = new JButton("Go to card2");
+    JButton btn2 = new JButton("Go to card3");
+    JButton btn3 = new JButton("Go to card1");
+
 }
