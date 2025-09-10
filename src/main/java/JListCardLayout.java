dbb4d43e8fe89458d7a79cf1e115165c6b9f7818
YANNICK kabunga <yankbg2@gmail.com>
2025-09-11 01:30:36 +0300

    add methods to manage button and panel additions in JListCardLayout

--- src/main/java/JListCardLayout.java
+++ src/main/java/JListCardLayout.java
@@ -12,5 +12,18 @@ public class JListCardLayout extends JFrame {
     JButton btn1 = new JButton("Go to card2");
     JButton btn2 = new JButton("Go to card3");
     JButton btn3 = new JButton("Go to card1");
+    // The creation of the addBtn method
+    public void addBtn(){
+        card1.add(btn1);
+        card2.add(btn2);
+        card3.add(btn3);
+    }
+    //The creation of the method mainPanelAdd
+    public void mainPanelAdd(){
+        mainPanel.add(card1, "card1");
+        mainPanel.add(card2, "card2");
+        mainPanel.add(card3, "card3");
+    }
+
 
 }
